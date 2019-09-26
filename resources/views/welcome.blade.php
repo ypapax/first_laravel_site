e7e68cab94042aa7fc79c75f0023810baa9cccb3
Maxim Yefremov <maxYefr@gmail.com>
2019-09-26 13:06:02 +0300

    pass data to view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,5 +4,10 @@
 @endsection
 @section('content')
     <h1>Welcome</h1>
+    <ul>
+			<?php foreach ($tasks as $task) : ?>
+        <li><?= $task; ?></li>
+			<?php endforeach; ?>
+    </ul>
 @endsection
 
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,14 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = [
+        'Go to the store',
+        'Go to the market',
+        'Go to work',
+    ];
+    return view('welcome', [
+        'tasks' => $tasks
+    ]);
 });
 
 
